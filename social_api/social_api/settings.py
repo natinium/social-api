395a578c99069b28e7600b8623d16cef6c460e64
Natnael Mesele <nmesele5@gmail.com>
2024-12-21 15:58:41 +0300

    fix host error

--- social_api/social_api/settings.py
+++ social_api/social_api/settings.py
@@ -115,8 +115,8 @@ DATABASES = {
         'NAME': os.environ["PGDATABASE"],
         'USER': os.environ["PGUSER"],
         'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'HOST': "autorack.proxy.rlwy.net:29551",
+        'PORT': "5432",
     }
 }
 
